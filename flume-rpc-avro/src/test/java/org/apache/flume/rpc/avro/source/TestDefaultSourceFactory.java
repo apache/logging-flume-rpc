2a571e67a588fd4c932ea6af21eb0addde180a95
Spotless <robots@logging.apache.org>
2026-06-26 17:16:16 +0200

    Format with Spotless
    Format the project with the Spotless configuration inherited from its
    parent.
    
    No other modifications are performed.

--- flume-rpc-avro/src/test/java/org/apache/flume/rpc/avro/source/TestDefaultSourceFactory.java
+++ flume-rpc-avro/src/test/java/org/apache/flume/rpc/avro/source/TestDefaultSourceFactory.java
@@ -1,22 +1,19 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package org.apache.flume.rpc.avro.source;
 
 import org.apache.flume.Source;
@@ -36,56 +33,50 @@ import org.junit.Test;
 
 public class TestDefaultSourceFactory {
 
-  private SourceFactory sourceFactory;
+    private SourceFactory sourceFactory;
 
-  @Before
-  public void setUp() {
-    sourceFactory = new DefaultSourceFactory();
-  }
+    @Before
+    public void setUp() {
+        sourceFactory = new DefaultSourceFactory();
+    }
 
-  @Test
-  public void testDuplicateCreate()  {
+    @Test
+    public void testDuplicateCreate() {
 
-    Source avroSource1 = sourceFactory.create("avroSource1", "avro");
-    Source avroSource2 = sourceFactory.create("avroSource2", "avro");
+        Source avroSource1 = sourceFactory.create("avroSource1", "avro");
+        Source avroSource2 = sourceFactory.create("avroSource2", "avro");
 
-    Assert.assertNotNull(avroSource1);
-    Assert.assertNotNull(avroSource2);
-    Assert.assertNotSame(avroSource1, avroSource2);
-    Assert.assertTrue(avroSource1 instanceof AvroSource);
-    Assert.assertTrue(avroSource2 instanceof AvroSource);
+        Assert.assertNotNull(avroSource1);
+        Assert.assertNotNull(avroSource2);
+        Assert.assertNotSame(avroSource1, avroSource2);
+        Assert.assertTrue(avroSource1 instanceof AvroSource);
+        Assert.assertTrue(avroSource2 instanceof AvroSource);
 
-    Source s1 = sourceFactory.create("avroSource1", "avro");
-    Source s2 = sourceFactory.create("avroSource2", "avro");
+        Source s1 = sourceFactory.create("avroSource1", "avro");
+        Source s2 = sourceFactory.create("avroSource2", "avro");
 
-    Assert.assertNotSame(avroSource1, s1);
-    Assert.assertNotSame(avroSource2, s2);
+        Assert.assertNotSame(avroSource1, s1);
+        Assert.assertNotSame(avroSource2, s2);
+    }
 
-  }
+    private void verifySourceCreation(String name, String type, Class<?> typeClass) throws Exception {
+        Source src = sourceFactory.create(name, type);
+        Assert.assertNotNull(src);
+        Assert.assertTrue(typeClass.isInstance(src));
+    }
 
-  private void verifySourceCreation(String name, String type,
-      Class<?> typeClass) throws Exception {
-    Source src = sourceFactory.create(name, type);
-    Assert.assertNotNull(src);
-    Assert.assertTrue(typeClass.isInstance(src));
-  }
-
-  @Test
-  public void testSourceCreation() throws Exception {
-    verifySourceCreation("seq-src", "seq", SequenceGeneratorSource.class);
-    verifySourceCreation("netcat-src", "netcat", NetcatSource.class);
-    verifySourceCreation("netcat-udp-src", "netcatudp", NetcatUdpSource.class);
-    verifySourceCreation("exec-src", "exec", ExecSource.class);
-    verifySourceCreation("avro-src", "avro", AvroSource.class);
-    verifySourceCreation("syslogtcp-src", "syslogtcp", SyslogTcpSource.class);
-    verifySourceCreation("multiport_syslogtcp-src", "multiport_syslogtcp",
-        MultiportSyslogTCPSource.class);
-    verifySourceCreation("syslogudp-src", "syslogudp", SyslogUDPSource.class);
-    verifySourceCreation("spooldir-src", "spooldir",
-        SpoolDirectorySource.class);
-    verifySourceCreation("http-src", "http", HTTPSource.class);
-    verifySourceCreation("custom-src", MockSource.class.getCanonicalName(),
-        MockSource.class);
-  }
-
+    @Test
+    public void testSourceCreation() throws Exception {
+        verifySourceCreation("seq-src", "seq", SequenceGeneratorSource.class);
+        verifySourceCreation("netcat-src", "netcat", NetcatSource.class);
+        verifySourceCreation("netcat-udp-src", "netcatudp", NetcatUdpSource.class);
+        verifySourceCreation("exec-src", "exec", ExecSource.class);
+        verifySourceCreation("avro-src", "avro", AvroSource.class);
+        verifySourceCreation("syslogtcp-src", "syslogtcp", SyslogTcpSource.class);
+        verifySourceCreation("multiport_syslogtcp-src", "multiport_syslogtcp", MultiportSyslogTCPSource.class);
+        verifySourceCreation("syslogudp-src", "syslogudp", SyslogUDPSource.class);
+        verifySourceCreation("spooldir-src", "spooldir", SpoolDirectorySource.class);
+        verifySourceCreation("http-src", "http", HTTPSource.class);
+        verifySourceCreation("custom-src", MockSource.class.getCanonicalName(), MockSource.class);
+    }
 }
--- flume-rpc-thrift/src/test/java/org/apache/flume/rpc/thrift/source/TestDefaultSourceFactory.java
+++ flume-rpc-thrift/src/test/java/org/apache/flume/rpc/thrift/source/TestDefaultSourceFactory.java
@@ -1,22 +1,19 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package org.apache.flume.rpc.thrift.source;
 
 import org.apache.flume.Source;
@@ -36,54 +33,49 @@ import org.junit.Test;
 
 public class TestDefaultSourceFactory {
 
-  private SourceFactory sourceFactory;
+    private SourceFactory sourceFactory;
 
-  @Before
-  public void setUp() {
-    sourceFactory = new DefaultSourceFactory();
-  }
+    @Before
+    public void setUp() {
+        sourceFactory = new DefaultSourceFactory();
+    }
 
-  @Test
-  public void testDuplicateCreate()  {
+    @Test
+    public void testDuplicateCreate() {
 
-    Source thriftSource1 = sourceFactory.create("thriftSource1", "thrift");
-    Source thriftSource2 = sourceFactory.create("thriftSource2", "thrift");
+        Source thriftSource1 = sourceFactory.create("thriftSource1", "thrift");
+        Source thriftSource2 = sourceFactory.create("thriftSource2", "thrift");
 
-    Assert.assertNotNull(thriftSource1);
-    Assert.assertNotNull(thriftSource2);
-    Assert.assertNotSame(thriftSource1, thriftSource2);
-    Assert.assertTrue(thriftSource1 instanceof ThriftSource);
-    Assert.assertTrue(thriftSource2 instanceof ThriftSource);
+        Assert.assertNotNull(thriftSource1);
+        Assert.assertNotNull(thriftSource2);
+        Assert.assertNotSame(thriftSource1, thriftSource2);
+        Assert.assertTrue(thriftSource1 instanceof ThriftSource);
+        Assert.assertTrue(thriftSource2 instanceof ThriftSource);
 
-    Source s1 = sourceFactory.create("thriftSource1", "thrift");
-    Source s2 = sourceFactory.create("thriftSource2", "thrift");
+        Source s1 = sourceFactory.create("thriftSource1", "thrift");
+        Source s2 = sourceFactory.create("thriftSource2", "thrift");
 
-    Assert.assertNotSame(thriftSource1, s1);
-    Assert.assertNotSame(thriftSource2, s2);
+        Assert.assertNotSame(thriftSource1, s1);
+        Assert.assertNotSame(thriftSource2, s2);
+    }
 
-  }
+    private void verifySourceCreation(String name, String type, Class<?> typeClass) throws Exception {
+        Source src = sourceFactory.create(name, type);
+        Assert.assertNotNull(src);
+        Assert.assertTrue(typeClass.isInstance(src));
+    }
 
-  private void verifySourceCreation(String name, String type,
-      Class<?> typeClass) throws Exception {
-    Source src = sourceFactory.create(name, type);
-    Assert.assertNotNull(src);
-    Assert.assertTrue(typeClass.isInstance(src));
-  }
-
-  @Test
-  public void testSourceCreation() throws Exception {
-    verifySourceCreation("seq-src", "seq", SequenceGeneratorSource.class);
-    verifySourceCreation("netcat-src", "netcat", NetcatSource.class);
-    verifySourceCreation("netcat-udp-src", "netcatudp", NetcatUdpSource.class);
-    verifySourceCreation("exec-src", "exec", ExecSource.class);
-    verifySourceCreation("syslogtcp-src", "syslogtcp", SyslogTcpSource.class);
-    verifySourceCreation("multiport_syslogtcp-src", "multiport_syslogtcp",
-        MultiportSyslogTCPSource.class);
-    verifySourceCreation("syslogudp-src", "syslogudp", SyslogUDPSource.class);
-    verifySourceCreation("http-src", "http", HTTPSource.class);
-    verifySourceCreation("thrift-src", "thrift", ThriftSource.class);
-    verifySourceCreation("custom-src", MockSource.class.getCanonicalName(),
-        MockSource.class);
-  }
-
+    @Test
+    public void testSourceCreation() throws Exception {
+        verifySourceCreation("seq-src", "seq", SequenceGeneratorSource.class);
+        verifySourceCreation("netcat-src", "netcat", NetcatSource.class);
+        verifySourceCreation("netcat-udp-src", "netcatudp", NetcatUdpSource.class);
+        verifySourceCreation("exec-src", "exec", ExecSource.class);
+        verifySourceCreation("syslogtcp-src", "syslogtcp", SyslogTcpSource.class);
+        verifySourceCreation("multiport_syslogtcp-src", "multiport_syslogtcp", MultiportSyslogTCPSource.class);
+        verifySourceCreation("syslogudp-src", "syslogudp", SyslogUDPSource.class);
+        verifySourceCreation("http-src", "http", HTTPSource.class);
+        verifySourceCreation("thrift-src", "thrift", ThriftSource.class);
+        verifySourceCreation("custom-src", MockSource.class.getCanonicalName(), MockSource.class);
+    }
 }
